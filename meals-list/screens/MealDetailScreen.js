1bd248166fdf6854c86b834e0e940af6c8f0fc9e
Sritharan Thiyagarajah <sk.sritharan75@gmail.com>
2023-12-28 21:02:42 +0530

    Navigation and styling

---
+++ meals-list/screens/MealDetailScreen.js
@@ -0,0 +1,79 @@
+import { useLayoutEffect } from "react";
+import { View, Image, Text, StyleSheet, ScrollView } from "react-native";
+
+import MealDetails from "../components/MealDetails";
+import { MEALS } from "../data/dummy-data";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
+import IconButton from "../components/IconButton";
+
+export default function MealDetailScreen({ route, navigation }) {
+  const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+    console.log("Pressed");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            onPress={headerButtonPressHandler}
+            icon={"heart"}
+            color={"white"}
+            size={24}
+          />
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
+
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listInnerContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 24,
+    margin: 8,
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listInnerContainer: {
+    maxWidth: "80%",
+  },
+});
